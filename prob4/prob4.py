fbdae4c2515f4224088871cbc69d8383bd37d83e
Alexis Gallagher <alexis@alexisgallagher.com>
2009-05-11 15:56:44 +0100

    reorg

--- prob4/prob4.py
+++ prob4/prob4.py
@@ -1,3 +1,14 @@
+from __future__ import absolute_import
+
+# begin: kludge to include ../eulertoolspy/functionalpy
+import os
+import sys
+eulerpath = os.path.abspath('..')
+if eulerpath not in sys.path:
+    sys.path = [eulerpath]+sys.path
+# end: kludge
+
+
 def isPalindrome(x):
     x = str(x)
     for i in xrange(len(x)):
@@ -6,7 +17,7 @@ def isPalindrome(x):
     return True
 
 def getpalindromes():
-    from functionalpy import icross
+    from eulertoolspy.functionalpy import icross, ifilter
     from itertools import takewhile, imap
     products = icross(xrange(100,999),xrange(100,999))
     products = ifilter(lambda (a,b): a<b, products)
