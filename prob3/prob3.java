a815fa3d2dbf468116117c2b15e3cb05696f23e7
Alexis Gallagher <alexis@alexisgallagher.com>
2009-05-26 20:25:34 +0100

    added factorInteger(:BigInteger):Map

--- prob3/prob3.java
+++ prob3/prob3.java
@@ -1,4 +1,5 @@
 import static java.lang.System.out;
+import java.lang.Integer;
 import java.math.BigInteger;
 
 import java.util.Map;
@@ -10,8 +11,24 @@ public class prob3
     {
 	String n = "600851475143";
 	out.println("biggest factor of " + n + " = " + biggestFactor(new BigInteger(n)));
+
+	out.println("factors of " + 12 + " = " + factorInteger(new BigInteger("12")));
     }
     
+    /** Returns the largest prime factor of n. 
+     *
+     * @param n value to be factored
+     * @return the largest factor of n
+     *
+     * 	This function takes [2,3,...,inf] as a list of candidate
+     * 	factors, and one by one tries to divide them out of n as many
+     * 	times as is possible. Since it tries repeatedly on a factor,
+     * 	it will automatically divide out powers of that factor. Thus,
+     * 	it will only divide out primes. Since the candidates are
+     * 	listed in order, the last factor to divide out successfuly
+     * 	will be the highest prime factor of n.
+     *
+     */
     public static BigInteger biggestFactor(BigInteger n) 
     {
 	// the largest factor so far
@@ -37,5 +54,36 @@ public class prob3
 	    candidate = candidate.add(BigInteger.ONE);
 	}
 	return factor;
+    }
+
+
+    /** Factors n into its prime factors
+     *
+     * @param n value to be factored
+     * @return Map from prime factors to their powers
+     *
+     */
+    public static Map factorInteger(BigInteger n) {
+	Map<BigInteger,Integer> factors = new HashMap<BigInteger,Integer>();
+
+	// start trying candidate factors
+	for(BigInteger candidate=new BigInteger("2"); 
+	    (n.compareTo( BigInteger.ONE ) == 1) 
+		&& ((candidate.pow(2)).compareTo( n.pow(2).add(BigInteger.ONE) ) == -1);
+	    candidate = candidate.add(BigInteger.ONE)) {
+
+	    // initialise factors to 0
+	    if( n.divideAndRemainder( candidate )[1].equals( BigInteger.ZERO ) ) {
+		factors.put(candidate,0);
+	    }
+	    
+	    // if candiate is a factor ..
+	    while( n.divideAndRemainder( candidate )[1].equals( BigInteger.ZERO ) ) {
+		// then factor out all its multiples
+		n = n.divideAndRemainder( candidate )[0];
+		factors.put(candidate, factors.get(candidate)+1);
+	    }
+	}
+	return factors;
     }
 }
