c8e7d16f549d01d31063313a3e6c98a18a279de9
Alexis Gallagher <alexis@alexisgallagher.com>
2009-06-10 13:51:55 +0100

    refactored out sieve

--- prob10/prob10.java
+++ prob10/prob10.java
@@ -4,32 +4,46 @@ public class prob10 {
 
     public static final int MAX = 2000000;
 
-    public static void main(String[] args) {
-	boolean[] primes = new boolean[MAX];
-	boolean[] visited = new boolean[MAX];
+    /** Sieves to find all primes up to n.
+
+	@param maximum number
+	@return a boolean[], where a[i] tells if i is prime 
+    */
+    public static boolean[] sieve(int n) 
+    {
+	boolean[] primes = new boolean[n];
+	boolean[] visited = new boolean[n];
 
 	Arrays.fill(primes,false);
 	Arrays.fill(visited,false);
 
-	long sum = 0;
-
 	primes[0]=false;
 	primes[1]=false;
 	// iterate thru every number
-	for(int i=2; i < MAX; ++i) {
+	for(int i=2; i < n; ++i) {
 	    if(visited[i]==true)
 		continue;
 	    // mark it as prime
 	    primes[i]=true;
-	    sum += i;
 	    // mark all its multiples as non-prime
-	    for(int j=(i+i); j < MAX; j=j+i) {
+	    for(int j=(i+i); j < n; j=j+i) {
 		primes[j]=false;
 		visited[j]=true;
 	    }
 	}
+	return primes;
+    }
 
+    public static void main(String[] args) {
+	boolean[] primes = sieve(MAX);
+
+	long sum = 0;
+	for(int i=1; i < MAX; ++i) {
+	    if(primes[i] == true)
+		sum += i;
+	}
 	System.out.println("The primes up to " + MAX + " sum to " + sum);
+
 	int count=1;
 	int i =1;
 	while(count < 1000) {
