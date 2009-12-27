4aa90290b928cbe8ec136fd4912990d98f240b26
Alexis Gallagher <alexis@alexisgallagher.com>
2009-12-27 16:00:35 +0000

    reformating. new problem

--- prob04/prob4.cpp
+++ prob04/prob4.cpp
@@ -3,33 +3,35 @@
 
 bool isPalindrome(int& x) 
 {
-  std::ostringstream stream;
-  stream << x;
-  const std::string s = stream.str();
-  
-  const int length  = s.length();
-  for(int i=0; i < length; ++i)
-    {
-      if(s[i] != s[length-1-i])
-	return false;
-    }
-  //  std::cout << s << " is a palindrome " << std::endl;
-  return true;
+     std::ostringstream stream;
+     stream << x;
+     const std::string s = stream.str();
+     
+     const int length  = s.length();
+     for(int i=0; i < length; ++i)
+     {
+	  if(s[i] != s[length-1-i])
+	       return false;
+     }
+     //  std::cout << s << " is a palindrome " << std::endl;
+     return true;
 }
 
 int main()
 {
-  int best = 0;
-  for(int i = 100; i < 1000; ++i) {
-    for(int j = 100; j < i; ++j) {
-      int product = i*j;
-      if(isPalindrome(product) == true && product > best)
-	{
-	  best = product;
-	  std::cout << "highest palindrome so far is " << i << " * " << j << " = " << best << std::endl;
-	}
-    }
-  }      
-  
-  std::cout << "highest 3-digit palindrom is best = " << best << std::endl;
+     int best = 0;
+     for(int i = 100; i < 1000; ++i) 
+     {
+	  for(int j = 100; j < i; ++j) 
+	  {
+	       int product = i*j;
+	       if(isPalindrome(product) == true && product > best)
+	       {
+		    best = product;
+		    std::cout << "highest palindrome so far is " << i << " * " << j << " = " << best << std::endl;
+	       }
+	  }
+     }      
+     
+     std::cout << "highest 3-digit palindrom is best = " << best << std::endl;
 }
