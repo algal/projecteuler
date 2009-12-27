32b27f77d49751ee81b49f029a55b861c10cb67e
Alexis Gallagher <alexis@alexisgallagher.com>
2009-12-27 16:58:18 +0000

    whitespace

--- prob01/prob1.cpp
+++ prob01/prob1.cpp
@@ -2,11 +2,11 @@
 
 int main()
 {
-  int sum=0;
-  for(int i=0; i<1000;++i)
-    {
-      if(i%3==0 || i%5==0)
-	sum+=i;
-    }
-  std::cout << "Sum = " << sum << std::endl;
+     int sum=0;
+     for(int i=0; i<1000;++i)
+     {
+	  if(i%3==0 || i%5==0)
+	       sum+=i;
+     }
+     std::cout << "Sum = " << sum << std::endl;
 }
