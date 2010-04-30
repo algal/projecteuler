252ed210e331b17fe589f63c681e04dc1adcdbba
Alexis Gallagher <alexis@alexisgallagher.com>
2010-04-30 13:39:28 +0100

    modified prob01 to print output

--- prob01/haskell/prob01.hs
+++ prob01/haskell/prob01.hs
@@ -1,1 +1,4 @@
 answer = sum [i | i <- [1..(1000-1)], (mod i 3 == 0) || (mod i 5 ==0)]
+
+main :: IO ()
+main = print answer
--- prob01/python/prob01.py
+++ prob01/python/prob01.py
@@ -1,1 +1,2 @@
-sum(i for i in range(1000) if i%3==0 or i%5==0)
+answer = sum(i for i in range(1000) if i%3==0 or i%5==0)
+print(answer)
