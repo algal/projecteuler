dc6b6a168d91d3a17cffe540a8a0a419458a4d4a
Alexis Gallagher <alexis@alexisgallagher.com>
2010-05-07 14:09:17 +0100

    added lisp/06

---
+++ lisp/prob06/prob06.lisp
@@ -0,0 +1,7 @@
+(defun foo ()
+  (- (expt (apply #'+ (loop :for i :from 1 :below 101 
+			 :collect i) ) 
+	   2)
+     (apply #'+ (loop :for i :from 1 :below 101 
+		   :collect (expt i 2)))))
+     
