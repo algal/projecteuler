3e6148e32c086426582744eb03563452c19b81be
Alexis Gallagher <alexis@alexisgallagher.com>
2010-05-14 12:59:43 +0100

    edits. revised divisibleBy. made scriptable

--- lisp/prob01/prob01.lisp
+++ lisp/prob01/prob01.lisp
@@ -1,13 +1,15 @@
-(defun divisibleBy (num factor)
+(defun divisibleBy (factor num)
   "Returns T if NUM is divisible by FACTOR"
   (zerop (mod num factor)))
 
 (defun foo ()
   "Returns sum of 1..1000, divisible by 3 and 5"
     (loop :for i :from 1 :to 1000
-	 :sum (if (and (divisibleBy i 3)
-			   (divisibleBy i 5))
+	 :sum (if (and (divisibleBy 3 i)
+			   (divisibleBy 5 i))
 		      i
 		      0)))
 
 (defparameter *answer* (foo))
+(format t "~A" *answer*)
+
