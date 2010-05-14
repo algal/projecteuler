ebf27cbba9b30213e7a75ba59a28c0f209352aac
Alexis Gallagher <alexis@alexisgallagher.com>
2010-05-14 13:29:09 +0100

    edits

--- lisp/prob01/prob01.lisp
+++ lisp/prob01/prob01.lisp
@@ -3,12 +3,12 @@
   (zerop (mod num factor)))
 
 (defun foo ()
-  "Returns sum of 1..1000, divisible by 3 and 5"
-    (loop :for i :from 1 :below 1000
-	 :sum (if (and (divisibleBy 3 i)
-			   (divisibleBy 5 i))
-		      i
-		      0)))
+  "Returns sum of [1,1000), divisible by 3 and 5"
+  (loop :for i :from 1 :below 1000
+     :sum (if (and (divisibleBy 3 i) 
+		   (divisibleBy 5 i))
+	      i
+	      0)))
 
 (defparameter *answer* (foo))
 (format t "~A~&" *answer*)
